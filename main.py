7ed33b20ce9c70e326cf750e713b2112420b508a
fahmed3 <fahmed3@stuy.edu>
2017-10-04 21:04:01 -0400

    Finished w/ Kristin

--- main.py
+++ main.py
@@ -6,20 +6,25 @@ form = Flask(__name__)
 def root():
     return render_template("root.html")
 
+#Correct username: softdev
+#Correct password: pd09
 @form.route('/result', methods = ['POST', 'GET'])
 def requests():
+    #either POST or GET method
     if request.method == 'POST':
         usr = request.form["usr"]
         pwd = request.form["pwd"]
     else:
         usr = request.args['usr']
         pwd = request.args['pwd']
+    #If username and password are correct, then results
     if usr == 'softdev' and pwd == 'pd09':
-        return render_template("result.html", usr)
+        return render_template("result.html", usr = usr)
+    #else send to error page, with booleans for password and username
     else:
-        return render_template("error.html",
-                               cUsr = (usr == 'softdev'),
-                               cPwd = (pwd == 'pd09'))
+        cUsr = (usr == 'softdev')
+        cPwd = (pwd == 'pd09')
+        return render_template("error.html", cUsr = cUsr, cPwd = cPwd)
 
 
 if __name__ == "__main__":
